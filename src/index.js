9d362a4f16f67296fc461002622299a8bfc68cf2
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-01-15 00:17:52 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const routes   = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
