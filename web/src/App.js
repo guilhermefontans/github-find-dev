fc404040a606c3cc78291ce62f7afa8a184246c5
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-01-16 23:04:44 -0300

    Criaão do layout web

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import './global.css';
 import './App.css';
+import './SideBar.css';
+import './Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input name="github_username" id="username_github" required/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required/>
+            </div>
+
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latidute</label>
+                <input name="latitude" id="latitude" required/>
+              </div>
+              <div className="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input name="longitude" id="longitude" required/>
+              </div>
+            </div>
+            <button type="submit">Salvar</button>
+          </form>
+        </aside>
+        <main>
+          <ul>
+            <li className="dev-item">
+              <header>
+                <img src="https://sdasdsadsadsa"/>
+                <div className="user-info">
+                  <strong>dasdasasdasdasds</strong>
+                  <span>sdasdsadsadsad</span>
+                </div>
+              </header>
+              <p>dsadsadsadsadsa</p>
+              <a href="dsadsadsadasds">Visualizar no GitHub</a>
+            </li>
+          </ul>
+        </main>
     </div>
   );
 }
